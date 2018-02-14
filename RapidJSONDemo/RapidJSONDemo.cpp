6998999937f0fcc6b40605ef34270e3aabc20b5a
windpenguin <windpenguin@gmail.com>
2018-02-14 17:44:35 +0800

    schema

--- RapidJSONDemo/RapidJSONDemo.cpp
+++ RapidJSONDemo/RapidJSONDemo.cpp
@@ -11,6 +11,10 @@
 #include <iostream>
 #include <string>
 #include <boost/locale/encoding.hpp>
+#include "rapidjson/error/en.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/schema.h"
+#include "rapidjson/stringbuffer.h"
 
 using namespace rapidjson;
 using std::string;
@@ -50,9 +54,67 @@ void MyTest()
 	std::cout << boost::locale::conv::between(roomName.GetString(), "GBK", "UTF8") << std::endl;
 }
 
+int TestSchema() {
+	// Read a JSON schema from file into Document
+	Document d;
+	char buffer[4096];
+
+	{
+		const char* pszSchemaDefs = "schemadefs.json";
+		FILE *fp = fopen(pszSchemaDefs, "r");
+		if (!fp) {
+			printf("Schema file '%s' not found\n", pszSchemaDefs);
+			return -1;
+		}
+		FileReadStream fs(fp, buffer, sizeof(buffer));
+		d.ParseStream(fs);
+		if (d.HasParseError()) {
+			fprintf(stderr, "Schema file '%s' is not a valid JSON\n", pszSchemaDefs);
+			fprintf(stderr, "Error(offset %u): %s\n",
+				static_cast<unsigned>(d.GetErrorOffset()),
+				GetParseError_En(d.GetParseError()));
+			fclose(fp);
+			return EXIT_FAILURE;
+		}
+		fclose(fp);
+	}
+
+	// Then convert the Document into SchemaDocument
+	SchemaDocument sd(d);
+
+	// Use reader to parse the JSON in stdin, and forward SAX events to validator
+	SchemaValidator validator(sd);
+	Reader reader;
+	FileReadStream is(stdin, buffer, sizeof(buffer));
+	if (!reader.Parse(is, validator) && reader.GetParseErrorCode() != kParseErrorTermination) {
+		// Schema validator error would cause kParseErrorTermination, which will handle it in next step.
+		fprintf(stderr, "Input is not a valid JSON\n");
+		fprintf(stderr, "Error(offset %u): %s\n",
+			static_cast<unsigned>(reader.GetErrorOffset()),
+			GetParseError_En(reader.GetParseErrorCode()));
+	}
+
+	// Check the validation result
+	if (validator.IsValid()) {
+		printf("Input JSON is valid.\n");
+		return EXIT_SUCCESS;
+	}
+	else {
+		printf("Input JSON is invalid.\n");
+		StringBuffer sb;
+		validator.GetInvalidSchemaPointer().StringifyUriFragment(sb);
+		fprintf(stderr, "Invalid schema: %s\n", sb.GetString());
+		fprintf(stderr, "Invalid keyword: %s\n", validator.GetInvalidSchemaKeyword());
+		sb.Clear();
+		validator.GetInvalidDocumentPointer().StringifyUriFragment(sb);
+		fprintf(stderr, "Invalid document: %s\n", sb.GetString());
+		return EXIT_FAILURE;
+	}
+}
+
 int main() 
 {
-	MyTest();
+	TestSchema();
 
 	system("pause");
 	return 0;
